151183ba579fcb25f1170f4edd8dc9da750d58c1
Spring Wedlund <wedlundspring@gmail.com>
2021-09-19 19:31:48 -0700

    deleted unnecessary files

--- js/charts.js
+++ js/charts.js
@@ -1,24 +1,129 @@
+function init() {
+  var selector = d3.select("#selDataset");
+
+  d3.json("samples.json").then((data) => {
+    var sampleNames = data.names;
+    sampleNames.forEach((sample) => {
+      selector
+        .append("option").text(sample).property('value', sample);
+    });
+    var initialSample = sampleNames[0];
+    buildMetadata(initialSample);
+    buildCharts(initialSample);
+});
+}
+
+init();
+
+function optionChanged(newSample) {
+  buildMetadata(newSample);
+  buildCharts(newSample);
+}
+// Print info the "Demographic Info" when a user chooses an ID number
+function buildMetadata(sample) {
+d3.json("samples.json").then((data) => {
+  var metadata = data.metadata;
+  var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
+  var pairs = Object.entries(resultArray[0]);
+  var PANEL = d3.select("#sample-metadata");
+
+  PANEL.html("");
+  var results = pairs.forEach(function (pair) {
+      PANEL.append("h6").text(pair[0] + ":" + pair[1]);
+  });  
+});
+}; 
+
 // Bar and Bubble charts
 // Create the buildCharts function.
-function buildCharts(sample) {
-  // Use d3.json to load and retrieve the samples.json file 
-  d3.json("samples.json").then((data) => {
-    
-
-    // Deliverable 1 Step 10. Use Plotly to plot the data with the layout. 
-
-    }
-    Plotly.newPlot('myDiv', data, layout); 
-
-    // 1. Create the trace for the bubble chart.
-    var bubbleData = [trace1];
-
-    // 2. Create the layout for the bubble chart.
-    var bubbleLayout = {
+function buildCharts(newSample) {
+  d3.json("samples.json").then(function ({samples,metadata }) {
+      var data = samples.filter((obj) => obj.id == sample) [0];
+      console.log(data);
+      // data for bar chart
+      var otuIDS = data.otu_ids.map((row) => `OUT ID: ${row}`);
+      var sampleValues = data.sample_values.slice(0, 10);
+      var sampleLabels = data.otu_labels.map((label) => 
+        label.replace(/\;/g, ', ')
+        );
+      // data for bubble chart
+      var otuID = data.otu_ids;
+      var sampleValue = data.sample_values;
+      var sampleLabel = data.otu_labels.map((label) =>
+        label.replace(/\;/g, ', ')
+        );
+      // data for gauge
+      var metaData = metadata.filter((obj) =>obj.id == sample)[0];
+      var washFreq = metaData.wfreq;
       
-    };
+
+      //data for bar chart
+      var data1 = [
+        {
+        x: sampleValues,
+        y: otuIDS,
+        type: 'bar',
+        orientation: 'h',
+        text: sampleLabels,
+        hoverinfo: 'text',
+        };
+      ];
+
+      // data for bubble chart
+      var data2 = [
+        {
+          x: otuID,
+          y: sampleValue,
+          mode: sampleLabel,
+          text: sampleLabel,
+          marker: {
+            size: sampleValue
+            color: otuID,
+          },
+        },
+      ];
+
+      // data for gauge chart
+      var data3 = [
+        {
+          // domain: washFreq,
+          value: washFreq,
+          title: {
+            text: 'Belly Buton Washing Frequency<br>Scrubs per Week',
+          },
+          type: 'indicator',
+          mode: 'gauge+number',
+          gauge: {
+            axis: { range: [null, 10] },
+          },
+        },
+      ];
+
+      // layout for bar chart
+      var layout1 = {
+        margin: {
+          t: 40,
+          l: 150,
+        },
+        title: {
+          text: 'Top 10 Bacterial Species (OTUs)',
+        },
+      };
+      //layout for bubble chart
+      var layout2 = {
+        xaxis: { title: 'OTU ID' },
+      };
+      //layout for gauge chart
+      var layout3 = {
+        width: 600,
+        height: 500,
+        margin: {t: 0, b: 0 },
+      };
+  
 
     // 3. Use Plotly to plot the data with the layout.
-    Plotly.newPlot(); 
+    Plotly.newPlot('bar', data1, layout1);
+    Plotly.newPlot('bubble', data2, layout2);
+    Plotly.newPlot('gauge', data3, layout3);
   });
 }
